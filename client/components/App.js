5ec1db418f0fd63d4630d542efc179704a0a2a30
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-07-06 18:18:57 +0400

    webpack issues fixed

--- client/components/App.js
+++ client/components/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => {
   return (
-    <h1>App!!!</h1>
+    <h1>Hello from App javascript</h1>
   )
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,18 +3,13 @@ import path from 'path'
 export default {
   entry: path.join(__dirname, '/client/index.js'),
   output: {
-    path: '/'
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'client'),
-        loaders: ['babel']
-      }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  },
-  resolve: {
-    extensions: ['.js']
   }
 }
